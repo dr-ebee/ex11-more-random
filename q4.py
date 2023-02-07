2c5921b2fc262b1469a05577ffdae4154606d840
Erin Bennett <ebennett@kehillah.org>
2023-02-07 07:58:35 -0800

    add q4

---
+++ q4.py
@@ -0,0 +1,16 @@
+# Keep flipping a coin until you get "tails"
+# Count how many heads you got
+
+def flip_a_coin():
+    """
+    Randomly return "heads" or "tails"
+    """
+    #### YOUR CODE HERE
+    return
+
+if __name__ == "__main__":
+    num_heads = 0
+    # Keep flipping a coint till you get "tails"
+    # Print out how many "heads" you got
+    #### YOUR CODE HERE
+
